786b2468d91641ab40e2b32583d4c3d566df6e06
Gleb Trubeev <gleb.trubeev@gmail.com>
2019-04-11 00:03:08 +0500

    new version

---
+++ app/src/main/java/com/example/myproject/DetailsAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.myproject
+import android.content.Intent
+import android.annotation.SuppressLint
+import android.content.Context
+import android.support.v4.content.ContextCompat.startActivity
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class DetailsAdapter : RecyclerView.Adapter<CustomViewHolder>() {
+    val listDetails = listOf("Общее","Двигатель","Подвеска и рулевое управление","Трансмиссия","Тормозная система","Электрооборудование")
+    override fun getItemCount(): Int {
+        return listDetails.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val Row = layoutInflater.inflate(R.layout.list_item,parent,false)
+        return CustomViewHolder(Row)
+    }
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+        val detailTitle = listDetails.get(position)
+        holder.view.car_detail?.text = detailTitle
+        if (position % 2 == 0) {
+            holder.view.car_detail.setBackgroundResource(R.color.colorPrimary)
+        }
+        else {
+            holder.view.car_detail.setBackgroundResource(R.color.cardview_shadow_start_color)
+        }
+
+        holder.view.setOnClickListener {
+        }
+    }
+
+
+
+
+}
+class CustomViewHolder (val view: View) : RecyclerView.ViewHolder(view) {
+
+}
--- app/src/main/java/com/example/myproject/MainActivity.kt
+++ app/src/main/java/com/example/myproject/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.myproject
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.recyclerview.extensions.ListAdapter
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.CardView
+import android.support.v7.widget.DividerItemDecoration
+import android.telecom.Call
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    var adapter:DetailsAdapter?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        adapter = DetailsAdapter()
+        list.layoutManager = LinearLayoutManager(applicationContext)
+        list.adapter = adapter
+        val itemDecoration =  DividerItemDecoration(applicationContext,LinearLayoutManager.VERTICAL)
+        itemDecoration.setDrawable(ColorDrawable(Color.BLUE))
+        list.addItemDecoration(itemDecoration)
+        list.background = ColorDrawable(Color.LTGRAY)
+
+
+
+
     }
+
+
+
 }
