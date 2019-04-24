2c1f8994959b91b8f7b9ee9836c72258a9906f86
Gleb Trubeev <gleb.trubeev@gmail.com>
2019-04-24 11:12:08 +0300

    new version

--- app/src/main/java/com/example/myproject/ContentActivity.kt
+++ app/src/main/java/com/example/myproject/ContentActivity.kt
@@ -3,60 +3,63 @@ import android.app.DownloadManager
 import android.widget.ImageView
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
+import android.view.KeyEvent
+import android.widget.TextView
+import android.widget.Toast
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_content.*
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.OkHttpClient
+import com.google.gson.reflect.TypeToken
+import okhttp3.*
+import kotlinx.coroutines.*
+import org.json.JSONArray
 import org.json.JSONObject
 import java.io.IOException
 import java.net.URL
+import kotlin.coroutines.CoroutineContext
+val data = mutableListOf<Response>()
+class ContentActivity : AppCompatActivity(),CoroutineScope {
+    private val rootJob = Job()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + rootJob
 
-class ContentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_content)
 
+            jsonParse()
 
-        jsonParse()
+                /*val kek = data
+        val xx = findViewById<TextView>(R.id.text_content)
+        for (i in kek) {
+                  for (k in i.employees) {
+                      xx.append(k.firstname)
+                  }
 
 
+        }
+        */
 
     }
-    fun jsonParse() {
-        val url = "https://api.myjson.com/bins/ao3lo"
-        val client = OkHttpClient()
-        //AsyncTaskHandJson().execute(url)
+    private fun jsonParse() = launch  {
+        val url = "https://api.myjson.com/bins/kp9wz"
+        val client = OkHttpClient.Builder().build()
         val request = okhttp3.Request.Builder().url(url).build()
-        client.newCall(request).enqueue(object: Callback {
-            override fun onResponse(call: Call, response: okhttp3.Response) {
-                val body = response.body()?.string()
-                println(body)
-                val gson = GsonBuilder().create()
-                val homefeed = Gson().fromJson(body,Cat::class.java)
-                runOnUiThread {
-
-                    text_content.text = homefeed.title
-                }
-            }
-            override fun onFailure(call: Call, e: IOException) {
-                println("Failed to execute request")
-            }
-        })
-
+        val response: String = withContext(Dispatchers.IO) {
+            client.newCall(request).execute().body()!!.string()
+        }
+        val type = object : TypeToken<ArrayList<Response>>() {}
+        val repos = Gson().fromJson<ArrayList<Response>>(response,type.type)
+        data.clear()
+        data.addAll(repos)
 
     }
-
-
-
-
-
 }
+data class Employee(val firstname: String,val age: Int,val mail: String)
+data class Response(val employees: List<Employee>)
 
-class Cat(val title: String,val text: String)
+
+
+
+
